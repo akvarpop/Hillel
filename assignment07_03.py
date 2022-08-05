6b13fec8fcc554a00dd2b5441d25d3926707af06
akvarpop <popravk@gmail.com>
2022-08-05 09:39:53 +0300

    add 07

--- assignment07_03.py
+++ assignment07_03.py
@@ -7,15 +7,41 @@
 #
 # підказка:
 #  %= N  # compound assignment
-N = int(input())
-K = int(input())
+
+# # Вариант 1
+# N = int(input())
+# K = int(input())
+#
+#
+# if K > N:
+#      K %= N
+#
+# new_list = [K for K in range(0, N, K)]
+#
+# print(new_list)
+
+# Вариант 2
+
+N = int(input('Ввести значення N: '))
+K = int(input('Ввести значення K: '))
 
 if K > N:
-    K %= N
-    my_list = list(range(N))
-    del my_list[K-1::K]
-    print(my_list)
-else:
-    my_list = list(range(N))
-    del my_list[K - 1::K]
-    print(my_list)
+    K = N % K
+
+my_list = [i for i in range(N+1)]
+del my_list[::K]
+
+print(my_list)
+
+# Мой первый вариант(без листа)
+
+
+# if K > N:
+#     K %= N
+#     my_list = list(range(N))
+#     del my_list[K-1::K]
+#     print(my_list)
+# else:
+#     my_list = list(range(N))
+#     del my_list[K - 1::K]
+#     print(my_list)
