3d91917b7e563e2035a2c06178cf98f6126c1b7e
akvarpop <popravk@gmail.com>
2022-08-12 11:17:34 +0300

    add 07

--- main.py
+++ main.py
@@ -1,58 +1,86 @@
-#!/usr/bin/python3
+class Human:
+    """Людина має"""
 
-# Assignment 03-01
-#
-# Завдання: задати формулу обчислення відстані L (замінити None на математичну формулу
-#           наведену нижче) - подальщий код приймає відповідне рішення - належить чи ні
-#           координата (Px, Py) до кільця (з центром у Cx, Cy та радіусами r1, r2).
-#
-# Варіанти виводу на екран:
-#
-#      1. "Координата (Px, Py) належить кільцю за центром у (Cx, Cy) та радіусами r1 = R1, r2 = R2"
-#      2. "Координата (Px, Py) не належить кільцю за центром у (Cx, Cy) та радіусами r1 = R1, r2 = R2"
-#
-# Формула відстані від заданої координати та координати центру кола:
-#
-#      L = sqrt((Px-Cx)^2 + (Py-Cy)^2),
-#
-#      якщо умова - r1 < L < r2 - виконується - точка належить кільцю,
-#      інакше - ні
-#
-#      де:
-#          L - відстань між двома точками з коордінатами
-#          Px, Py - координата точки
-#          Cx, Cy - координата центру кола r1 та r2
-#          r1, r2 - радіус внутрішньго/зовнішнього кола
-#
-# Приклад приналежності:
-#
-# Cx = 0, Cy = 0
-# r1 = 3
-# r2 = 7
-# Px = 4, Py = 5
-#
-# L = sqrt((4-0)^2 + (5-0)^2) == 6.4
-#
-# "Координата (4, 5) належить кільцю з центром у (0, 0) та радіусами r1 = 3, r2 = 7"
-#
-# Підказка:
-#     * sqrt - через пайтонівські аріфметичні операціх: N**0.5, 4**0.5 == 2, 16**0.5 == 4...
+    def __init__(self, first_name: str, last_name: str, date_birthday: int, gender: str):
+        """Параметри людини"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.date_birthday = date_birthday
+        self.gender = gender
+        self.energy = 100
 
-from math import sqrt
+    def human_eat(self):
+        """Коли людина їсть енергія зростає"""
+        self.energy += 5
 
-Px = int(input("Введіть Px: "))  # зчитування значення від користувача та конвертація у число (int)
-Py = int(input("Введіть Py: "))
-Cx = int(input("Введіть Сx: "))
-Cy = int(input("Введіть Сy: "))
-r1 = int(input("Введіть r1: "))
-r2 = int(input("Введіть r2: "))
+    def human_sleep(self):
+        """Коли людина спить енергія зростає"""
+        self.energy += 10
 
-L = sqrt((Px-Cx) ** 2 + (Py-Cy) ** 2)
-print(L)
+    def human_talk(self):
+        """Коли людина говорить енергія зменшуэтся"""
+        self.energy -= 5
 
-result = "не належить"
-if r1 < L < r2:
-   result = "належить"
+    def human_walk(self):
+        """Коли людина ходить енергія зменшуэтся"""
+        self.energy -= 10
 
-print(f"Координата ({Px}, {Py}) {result} кільцю за центром у ({Cx}, {Cy}) та радіусами r1 = {r1}, r2 = {r2}")
+    def human_do_home_work(self):
+        """Коли людина робить домашку енергія зменшуэтся"""
+        self.energy -= 90
 
+
+# class Child(Human):
+#     def __init__(self):
+#         super().__init__(self.energy)
+#         self.count = max(man_1.energy, man_2.energy, man_3.energy, woman_1.energy, woman_2.energy)
+
+
+
+if __name__ == "__main__":
+
+    man_1 = Human(first_name='Vasilyi', last_name='Groot', date_birthday=29, gender='Male')
+    man_2 = Human(first_name='Roman', last_name='Pool', date_birthday=25, gender='Male')
+    man_3 = Human(first_name='Serega', last_name='Pupkin', date_birthday=44, gender='Male')
+    woman_1 = Human(first_name='Tonya', last_name='Rokojop', date_birthday=19, gender='Female')
+    woman_2 = Human(first_name='Nata', last_name='Vernaja', date_birthday=28, gender='Female')
+
+    man_1.human_eat(), man_1.human_sleep()
+    man_2.human_sleep(), man_2.human_sleep()
+    man_3.human_walk(), man_3.human_eat()
+    woman_1.human_talk(), woman_1.human_sleep()
+    woman_2.human_do_home_work(), woman_2.human_talk()
+
+
+
+    print(man_1.first_name, man_1.last_name, man_1.date_birthday, man_1.gender, man_1.energy)
+    print(man_2.first_name, man_2.last_name, man_2.date_birthday, man_2.gender, man_2.energy)
+    print(man_3.first_name, man_3.last_name, man_3.date_birthday, man_3.gender, man_3.energy)
+    print(woman_1.first_name, woman_1.last_name, woman_1.date_birthday, woman_1.gender, woman_1.energy)
+    print(woman_2.first_name, woman_2.last_name, woman_2.date_birthday, woman_2.gender, woman_2.energy)
+
+    '''Или можно'''
+    print(man_1.__dict__)
+    print(man_2.__dict__)
+    print(man_3.__dict__)
+    print(woman_1.__dict__)
+    print(woman_2.__dict__)
+
+
+    count = max(man_1.energy, man_2.energy, man_3.energy, woman_1.energy, woman_2.energy)
+
+    if man_1.energy == count:
+        print(f'Найбільше енергії лишилося: '
+              f'{man_1.first_name, man_1.last_name, man_1.date_birthday, man_1.gender, man_1.energy}')
+    elif man_2.energy == count:
+        print(f'Найбільше енергії лишилося: '
+              f'{man_2.first_name, man_2.last_name, man_2.date_birthday, man_2.gender, man_2.energy}')
+    elif man_3.energy == count:
+        print(f'Найбільше енергії лишилося: '
+              f'{man_3.first_name, man_3.last_name, man_3.date_birthday, man_3.gender, man_3.energy}')
+    elif woman_1.energy == count:
+        print(f'Найбільше енергії лишилося: '
+              f'{woman_1.first_name, woman_1.last_name, woman_1.date_birthday, woman_1.gender, woman_1.energy}')
+    elif woman_2.energy == count:
+        print(f'Найбільше енергії лишилося: '
+              f'{woman_2.first_name, woman_2.last_name, woman_2.date_birthday, woman_2.gender, woman_2.energy}')
